ed4d8c7146ec36b136f13224479d4f8180d2d6c1
AntoineMeunier52 <meunier.antoine52@gmail.com>
2025-06-01 16:59:19 +0200

    learn route

---
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2>Lesson</h2>
+    <p>this is a lesson</p>
+    <p>chapter slug: {{ $route.params.chapterSlug }}</p>
+    <p>Lesson slug: {{ $route.params.lessonSlug }}</p>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
